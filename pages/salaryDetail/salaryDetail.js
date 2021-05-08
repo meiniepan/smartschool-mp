24ad43992702397187f1357d55af2016bd47312d
Burning <meiniepan@163.com>
2021-05-08 12:20:14 +0800

    fix 工资条详情

--- pages/salaryDetail/salaryDetail.js
+++ pages/salaryDetail/salaryDetail.js
@@ -1,18 +1,22 @@
 // pages/salaryDetail/salaryDetail.js
 let app = getApp();
+const globalData = getApp().globalData;
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        mData: {}
+        mData: {
+            title:app.globalData.salaryTitle
+        }
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        console.log("id", options.id)
         this.setData({
             id: options.id,
         })
@@ -27,12 +31,28 @@ Page({
         app.httpPost('/api/v17/admin/wages/detail', data).then((res) => {
             console.log("data", res)
             let mData = res.respResult;
-
+            // mData.expand.remark.forEach(it => {
+            //     it.show = false
+            // })
             this.setData({
                 mData,
             })
 
         });
+    },
+    doRemark(e){
+        let index =  e.currentTarget.dataset.index
+        let mData = this.data.mData
+        var b = mData.expand.remark[index].show
+        mData.expand.remark[index].show = !b
+        this.setData({
+            mData,
+        })
+    },
+    doFinish(){
+        wx.navigateBack({
+            delta: 1,
+        })
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
