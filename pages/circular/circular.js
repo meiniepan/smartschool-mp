6c944a5fd01be91deacf26b985e811295cd4e871
Burning <meiniepan@163.com>
2022-03-11 17:54:50 +0800

    fix 播放音频

--- pages/circular/circular.js
+++ pages/circular/circular.js
@@ -24,158 +24,6 @@ Page({
         app.checkUpdate()
         this.refresh()
         this.getSemester()
-        this.nfcRead()
-    },
-
-    nfcRead() {
-
-        console.log('nfc')
-
-        const nfc = wx.getNFCAdapter()
-
-        this.nfc = nfc
-
-        let _this = this
-
-
-
-        function discoverHandler(res) {
-
-            console.log('discoverHandler', res)
-
-            const data = new Uint8Array(res.id)
-
-            let str = ""
-
-            data.forEach(e => {
-
-                let item = e.toString(16)
-
-                if (item.length == 1) {
-
-                    item = '0' + item
-
-                }
-
-                item = item.toUpperCase()
-
-                console.log(item)
-
-                str += item
-
-            })
-
-            _this.setData({
-
-                newCardCode: str
-
-            })
-
-            console.log(str)
-
-            wx.showToast({
-
-                title: '读取成功！',
-
-                icon: 'none'
-
-            })
-            showModal(str)
-
-        }
-
-        nfc.startDiscovery({
-
-            success(res) {
-
-                console.log(res)
-
-                wx.showToast({
-
-                    title: 'NFC读取功能已开启！',
-
-                    icon: 'none'
-
-                })
-
-                nfc.onDiscovered(discoverHandler)
-
-            },
-
-            fail(err) {
-
-                console.log('failed to discover:', err)
-
-                if(!err.errCode){
-
-                    wx.showToast({
-
-                        title: '请检查NFC功能是否正常!',
-
-                        icon: 'none'
-
-                    })
-
-                    return
-
-                }
-
-                switch (err.errCode) {
-
-                    case 13000:
-
-                        wx.showToast({
-
-                            title: '设备不支持NFC!',
-
-                            icon: 'none'
-
-                        })
-
-                        break;
-
-                    case 13001:
-
-                        wx.showToast({
-
-                            title: '系统NFC开关未打开!',
-
-                            icon: 'none'
-
-                        })
-
-                        break;
-
-                    case 13019:
-
-                        wx.showToast({
-
-                            title: '用户未授权!',
-
-                            icon: 'none'
-
-                        })
-
-                        break;
-
-                    case 13010:
-
-                        wx.showToast({
-
-                            title: '未知错误!',
-
-                            icon: 'none'
-
-                        })
-
-                        break;
-
-                }
-
-            }
-
-        })
-
     },
 
     /**
@@ -189,6 +37,13 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+
+    },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
 
     },
 
@@ -451,13 +306,6 @@ Page({
 
 
     /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
@@ -471,7 +319,7 @@ Page({
         return {
             title: '汇文云',
             path: 'pages/splash/splash',
-            imageUrl:"../../assets/images/bac_share.png",
+            imageUrl: "../../assets/images/bac_share.png",
         }
     }
 })
